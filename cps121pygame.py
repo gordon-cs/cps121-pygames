9bdd597620e52e040678b8642d7630e3d85cfd11
Jonathan Senning <jonathan.senning@gordon.edu>
2024-07-09 08:39:12 -0400

    added window close event handling

--- cps121pygame.py
+++ cps121pygame.py
@@ -1,4 +1,5 @@
-import os
+import os, time
+import threading
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "yes"
 import pygame as pg
 
@@ -11,7 +12,30 @@ magenta = (255,  0,255)
 yellow  = (255,255,  0)
 white   = (255,255,255)
 
+class EventMonitor(threading.Thread):
+    """Thread class to handle window close event"""
+
+    running = False
+
+    def __init__(self, daemon=True):
+        """Instantiate daemon thread"""
+        threading.Thread.__init__(self, daemon=True)
+
+    def run(self):
+        """Polls pygame event queue and handles window close events"""
+        self.running = True
+        while self.running:
+            time.sleep(0.01)
+            if not pg.display.get_init():
+                pg.display.init()
+            for event in pg.event.get():
+                if event.type == 32787: # WindowClose event
+                    # Want window to disappear but shouldn't call pg.quit()
+                    pg.display.set_mode((0,0), flags=pg.HIDDEN)
+                    self.running = False
+
 class Picture:
+    monitor = None
     window = None
     image = None
     size = None
@@ -67,59 +91,84 @@ class Picture:
 
     def get_color(self, x, y):
         return self.image.get_at((x, y))[0:3]
-    
-#    def get_red(self, x, y):
-#        return self.image.get_at((x,y))[0]
 
-#    def get_green(self, x, y):
-#        return self.image.get_at((x,y))[1]
+    def get_red(self, x, y):
+        return self.image.get_at((x,y))[0]
 
-#    def get_blue(self, x, y):
-#        return self.image.get_at((x,y))[2]
+    def get_gree(self, x, y):
+        return self.image.get_at((x,y))[1]
 
-#    def set_red(self, x, y, value):
-#        c = self.image.get_at((x,y))[0:3]
-#        self.image.set_at((x, y), (value, c[1], c[2]))
+    def get_blue(self, x, y):
+        return self.image.get_at((x,y))[2]
 
-#    def set_green(self, x, y, value):
-#        c = self.image.get_at((x,y))[0:3]
-#        self.image.set_at((x, y), (c[0], value, c[2]))
+    def set_red(self, x, y, value):
+        c = self.image.get_at((x,y))[0:3]
+        self.image.set_at((x, y), (value, c[1], c[2]))
 
-#    def set_blue(self, x, y, value):
-#        c = self.image.get_at((x,y))[0:3]
-#        self.image.set_at((x, y), (c[0], c[1], value))
+    def set_green(self, x, y, value):
+        c = self.image.get_at((x,y))[0:3]
+        self.image.set_at((x, y), (c[0], value, c[2]))
+
+    def set_blue(self, x, y, value):
+        c = self.image.get_at((x,y))[0:3]
+        self.image.set_at((x, y), (c[0], c[1], value))
 
     def copy(self):
+        """Returns a new copy of an image"""
         return Picture(self)
     
     def copyInto(self, pic, x, y):
+        """Copies pic into the image with upper-left at (x,y)"""
         self.image.blit(pic.image, (x, y))
-
-#    def copyOutof(self, pic, x, y, w, h):
         
     def magnify(self):
+        """Returns magnified image and image size
+        
+        Returns the scaled image and the true size (in pixels)
+        of the image.
+        """
         orig_size = self.image.get_size()
         mag_size = (orig_size[0] * self.magnification,
                     orig_size[1] * self.magnification)
         mag_image = pg.transform.scale(self.image, mag_size)
         return mag_image, mag_size
-    
+
     def show(self, title=None):
+        # start event monitor if it's not running
+        if self.monitor is None or not self.monitor.running:
+            self.monitor = EventMonitor()
+            self.monitor.start()
+
+        # get image and image size and create window
+        mag_image, mag_size = self.magnify()
+        self.window = pg.display.set_mode(mag_size, flags=pg.SHOWN)
+
+        # display image
+        self.window.blit(mag_image, (0, 0))
         if title is not None:
             self.title = title
-        mag_image, mag_size = self.magnify()
-        self.window = pg.display.set_mode(mag_size)
-        self.window.blit(mag_image, (0, 0))
         pg.display.set_caption(self.title)
         pg.display.update()
+        pg.event.pump()
 
     def repaint(self, title=None):
+        # start event monitor if it's not running
+        if self.monitor is None or not self.monitor.running:
+            self.monitor = EventMonitor()
+            self.monitor.start()
+
+        # get image and image size, and make it shown if previously hidden
+        mag_image, mag_size = self.magnify()
+        if not pg.display.get_active():
+            pg.display.set_mode(mag_size, flags=pg.SHOWN)
+
+        # update image
+        self.window.blit(mag_image, (0, 0))
         if title is not None:
             self.title = title
-        mag_image, mag_size = self.magnify()
-        self.window.blit(mag_image, (0, 0))
-        pg.display.set_caption(self.title)
+            pg.display.set_caption(self.title)
         pg.display.update()
+        pg.event.pump()
     
     def close(self):
         pg.quit()
@@ -163,6 +212,11 @@ if __name__ == "__main__":
     nico = Picture('nico.jpg')
     nico.show()
     input('press Enter to continue...')
+
+    for x in range(100, 200):
+        r = nico.get_red(x, 100)
+        r = 255-r
+        nico.set_red(x, 100, r)
 
     nico.set_magnification(2)
     nico.repaint()
